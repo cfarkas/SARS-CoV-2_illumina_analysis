6c0e9142d7882e939a7d122f5daa43d4641d56f4
cfarkas <carlosfarkas@gmail.com>
2020-04-27 22:56:28 -0600

    Update SARS-CoV-2_get_ngs.sh

--- SARS-CoV-2_get_ngs.sh
+++ SARS-CoV-2_get_ngs.sh
@@ -9,7 +9,7 @@ path_to_perl5_lib=${4}
 
 if [ "$1" == "-h" ]; then
   echo ""
-  echo "Usage: ./`basename $0` [SRA_list] [Reference] [Threads] [path_to_perl5_lib]"
+  echo "Usage: ./`basename $0` [SRA_list] [Reference] [Threads]"
   echo ""
   echo "This program will call variants using SAMtools/bcftools in given SRA NGS sequences files to obtain viral founder variants."
   echo ""
@@ -19,7 +19,6 @@ if [ "$1" == "-h" ]; then
   echo ""
   echo "[Threads]: Number of CPUs for the task (integer)"
   echo ""
-  echo "[path_to_perl5_lib]: Path to PERL5LIB, in VCFtools folder. If vcftools is installed in /home/user/, will be: /home/user/vcftools/src/perl/ "
   exit 0
 fi
 
@@ -151,7 +150,7 @@ for f in *.bam; do samtools index ${f}; done
 ### Performing Germline Variant Calling with strelka v2.9.2 ###
 ###############################################################
 
-echo "Performing Germnline Variant Calling with strelka v2.9.2:"
+echo "Performing Variant Calling with strelka v2.9.2:"
 echo ""
 echo "for documentation, please see: https://github.com/Illumina/strelka"
 echo ""
@@ -159,57 +158,92 @@ echo "downloading strelka binary from github repository"
 wget https://github.com/Illumina/strelka/releases/download/v2.9.2/strelka-2.9.2.centos6_x86_64.tar.bz2
 tar xvjf strelka-2.9.2.centos6_x86_64.tar.bz2
 
+echo "Calling Founder Variants"
 bam= ls -1 *.bam
-for bam in *.bam; do
-# configuration
-begin=`date +%s`
+for bam in *.bam; do 
 ./strelka-2.9.2.centos6_x86_64/bin/configureStrelkaGermlineWorkflow.py \
-    --bam SRR10971381.bam \
+    --bam SRR10971381.sorted.bam \
     --bam ${bam} \
     --referenceFasta ${2} \
-    --runDir ${bam}_strelka
-# execution on a single local machine with n parallel jobs
-${bam}_strelka/runWorkflow.py -m local -j ${3}
-echo "Variant Calling done"
-end=`date +%s`
-elapsed=`expr $end - $begin`
-echo "................................................................................."
-cp ./${bam}_strelka/results/variants/variants.vcf.gz ./strelka_germline_variants.vcf.gz
-bgzip -f -d strelka_germline_variants.vcf.gz
+    --runDir ${bam}.founder
+# execution on a single local machine with 20 parallel jobs
+${bam}.founder/runWorkflow.py -m local -j ${3}
+cp ./${bam}.founder/results/variants/variants.vcf.gz ./strelka_germline_variants.vcf.gz
+bgzip -d strelka_germline_variants.vcf.gz
 grep "#" strelka_germline_variants.vcf > strelka_germline_variants_header.vcf
 grep "PASS" strelka_germline_variants.vcf > strelka_germline_variants_PASS.vcf
-cat strelka_germline_variants_header.vcf strelka_germline_variants_PASS.vcf > ${bam}.vcf
-rm strelka_germline_variants_header.vcf strelka_germline_variants_PASS.vcf strelka_germline_variants.vcf.gz
+grep -v "NoPassedVariantGTs" strelka_germline_variants_PASS.vcf > strelka_germline_variants_PASS2.vcf
+rm strelka_germline_variants_PASS.vcf
+cat strelka_germline_variants_header.vcf strelka_germline_variants_PASS2.vcf > strelka_germline_variants.filtered.vcf
+rm strelka_germline_variants_header.vcf strelka_germline_variants_PASS2.vcf
+mv strelka_germline_variants.filtered.vcf ./${bam}.founder.vcf
+rm strelka_germline_variants.vcf
+rm -r -f ${bam}.founder
 done
 
-###########################
-# Cleaning Up directories #
-###########################
+echo "Calling Somatic Variants"          
+bam= ls -1 *.bam
+for bam in *.bam; do
+./strelka-2.9.2.centos6_x86_64/bin/configureStrelkaSomaticWorkflow.py \
+    --normalBam SRR10971381.sorted.bam \
+    --tumorBam ${bam} \
+    --referenceFasta ${2} \
+    --runDir ${bam}.somatic
+# execution on a single local machine with n parallel jobs
+${bam}.somatic/runWorkflow.py -m local -j ${3}
+# snvs
+cp ./${bam}.somatic/results/variants/somatic.snvs.vcf.gz ./strelka_somatic_variants.vcf.gz
+cp ./${bam}.somatic/results/variants/somatic.indels.vcf.gz ./strelka_somatic_indels.vcf.gz
+bgzip -d strelka_somatic_variants.vcf.gz
+grep "#" strelka_somatic_variants.vcf > strelka_somatic_variants_header.vcf
+grep "PASS" strelka_somatic_variants.vcf > strelka_somatic_variants_PASS.vcf
+grep -v "NoPassedVariantGTs" strelka_somatic_variants_PASS.vcf > strelka_somatic_variants_PASS2.vcf
+rm strelka_somatic_variants_PASS.vcf
+cat strelka_somatic_variants_header.vcf strelka_somatic_variants_PASS2.vcf > strelka_somatic_variants.filtered.vcf
+rm strelka_somatic_variants_header.vcf strelka_somatic_variants_PASS2.vcf
+mv strelka_somatic_variants.filtered.vcf ./${bam}.somatic.snvs.vcf
+rm strelka_somatic_variants.vcf
+# indels
+bgzip -d strelka_somatic_indels.vcf.gz
+grep "#" strelka_somatic_indels.vcf > strelka_somatic_indels_header.vcf
+grep "PASS" strelka_somatic_indels.vcf > strelka_somatic_indels_PASS.vcf
+grep -v "NoPassedVariantGTs" strelka_somatic_indels_PASS.vcf > strelka_somatic_indels_PASS2.vcf
+rm strelka_somatic_indels_PASS.vcf
+cat strelka_somatic_indels_header.vcf strelka_somatic_indels_PASS2.vcf > strelka_somatic_indels.filtered.vcf
+rm strelka_somatic_indels_header.vcf strelka_somatic_indels_PASS2.vcf
+mv strelka_somatic_indels.filtered.vcf ./${bam}.somatic.indels.vcf
+rm strelka_somatic_indels.vcf
+rm -r -f ${bam}.somatic
+done
+
+#########################
+# Cleaning Up SAM files #
+#########################
 
 echo "Cleaning up strelka directories and intermediate files"
-rm -r -f *_strelka strelka_germline_variants.vcf strelka_germline_variants_PASS.vcf strelka_germline_variants_header.vcf
+rm *.sam
 
-#######################################
-### Merging variants using jacquard ###
-#######################################
+###############################################
+### Merging founder variants using jacquard ###
+###############################################
 echo "Merging variants using jacquard"
 echo ""
 echo "for information, please see: https://jacquard.readthedocs.io/en/v0.42/overview.html#why-would-i-use-jacquard"
 mkdir to_translate
-cp *.vcf ./to_translate/
+cp *.founder.vcf ./to_translate/
 cd to_translate
 jacquard translate --force ./ translated_vcfs
 cd ..
 cp ./to_translate/translated_vcfs/* ./
 mkdir primary_vcfs
-mv *.bam.vcf ./primary_vcfs/
+mv *founder.vcf ./primary_vcfs/
 mkdir to_merge
 mv *.translatedTags.vcf ./to_merge/
 cd to_merge
 jacquard merge ./ merged.vcf
 cd ..
 cp ./to_merge/merged.vcf ./
-echo "All done"
+echo "All done. Merged vcf is called merged.vcf and is located in current directory"
 
 ###############################################################
 #
